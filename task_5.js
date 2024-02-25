b1d14fbce4384b371f0c8facd87f6fc26581d9e8
nekaoganezovva <nekaoganezova@gmail.com>
2024-02-25 18:39:59 +0400

    done

--- task_5.js
+++ task_5.js
@@ -1,3 +1,18 @@
 /**
  * დაწერეთ ფუნქცია, რომელიც "string" -ების "array" -ში დათვლის საშუალოდ რა ზომის "string" -ებია
  */
+
+function avLength(stringsArray) {
+    if (stringsArray.length === 0) {
+        return 0;
+    }
+
+    const totalLength = stringsArray.reduce((sum, str) => sum + str.length, 0);
+    return totalLength / stringsArray.length;
+}
+
+// Example usage
+const exampleArray = ["qwertyuiop", "qwertyu", "qwert", "qwe"];
+const averageLength = avLength(exampleArray);
+
+console.log(averageLength);
